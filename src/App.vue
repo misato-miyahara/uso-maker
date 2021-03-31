a860da08da260b44c9ed8963be309c909bd15872
misato-miyahara <m.miyahara@sanchi.dev>
2021-04-01 07:45:52 +0900

    first commit.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,129 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!-- CSS設計めんどくさいみょーん -->
+<!-- PRECSSとインラインCSSが混ざってますが許してちょんまげ -->
+<div class="ly_wrapper">
+  <div style="text-align: center;">
+    <h1 class="el_rainbow">超絶面白いうそメーカーを作りました！！</h1>
+  </div>
+  <div class="bl_maker">
+    <p style="
+      font-size: 1.2rem;
+      font-weight: bold;
+    ">
+      じつは私...
+    </p>
+    <ul class="bl_maker_slots">
+      <li>
+        <Slot :items="items[0]" />
+      </li>
+      <li>
+        <Slot :items="items[1]" />
+      </li>
+      <li>
+        <Slot :items="items[2]" />
+      </li>
+    </ul>
+    <p style="
+      text-align: right;
+      font-size: 1.2rem;
+      font-weight: bold;
+    "
+    >
+      なんです！！
+    </p>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Slot from './components/Slot.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Slot
+  },
+  data () {
+    return {
+      items: [
+        [
+          '超絶可愛い',
+          'ちょっと臭い',
+          '最強で',
+          'スキンヘッドで',
+          'モヒカンで',
+          '膝裏に興奮する'
+        ],
+        [
+          '無人島住みの',
+          '健気な',
+          '頭のおかしな',
+          '厨二病な',
+          'パンツを被る',
+          '泣き虫な',
+          '厨二病な',
+          '生きて腸に届く'
+        ],
+        [
+          '女の子',
+          '男の子',
+          '男の娘',
+          'サディスト',
+          'ド変態',
+          'エクソシスト',
+          '殺人鬼',
+          '長男',
+          '末っ子長男',
+          '童貞',
+          '凡人',
+          'ガキンチョ',
+          '社長令嬢ですの',
+          '主人公',
+          'ビフィズス菌',
+          '大腸菌'
+        ]
+      ]
+    }
+  },
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;800&display=swap'
+      }
+    ]
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ul {
+  list-style: none;
+}
+</style>
+
+<style scoped>
+html {
+  font-size: 62.5%;
+}
+body {
+  font-family: 'M PLUS 1p', sans-serif;
+  font-size: 1.4em;
+}
+.bl_maker_slots {
+  display: grid;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+}
+.el_rainbow {
+  color: #FF8C00;
+  background: -webkit-linear-gradient(0deg, #0051ff, #ff0095, #09ff00, #eeff00, #ff3232);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+@media screen and (max-width: 800px) {
+  .bl_maker_slots {
+    display: grid;
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
 }
 </style>
---
+++ src/components/Slot.vue
@@ -0,0 +1,84 @@
+<template>
+<div class="bl_slot">
+  <p class="el_slot_txt" style="text-align: center;">
+    <template v-if="stopFlg">
+      うんち！💩
+    </template>
+    <template v-else>
+      {{ items[itemIdx] }}
+    </template>
+  </p>
+  <div>
+    <button
+      @click="stopTimer"
+      class="el_slot_btn"
+    >
+      STOP
+    </button>
+  </div>
+</div>
+<!-- /.bl_slot -->
+</template>
+
+<script>
+export default {
+  name: 'Slot',
+  props: {
+    items: Array
+  },
+  data () {
+    return {
+      itemIdx: 0,
+      timer: null,
+      stopFlg: false
+    }
+  },
+  mounted () {
+    this.startTimer()
+  },
+  unmounted () {
+    this.stopTimer
+  },
+  methods: {
+    countUp () {
+      if (++this.itemIdx >= this.items.length) {
+        this.itemIdx = 0
+      }
+    },
+    startTimer () {
+      this.timer = setInterval(this.countUp, 80)
+    },
+    stopTimer () {
+      this.stopFlg = true
+      clearInterval(this.timer)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bl_slot {
+}
+.el_slot_txt {
+  font-size: 2rem;
+  font-weight: bold;
+  white-space: nowrap;
+}
+.el_slot_btn {
+  margin: auto;
+  display: block;
+  width: 80px;
+  height: 80px;
+  font-weight: bold;
+  color: rgb(8, 136, 255);
+  border-radius: 100%;
+  border-color: blue;
+  background-color: rgb(220, 255, 247);
+  cursor: pointer;
+  box-shadow: 0px 0px 16px -6px rgba(0,0,0,0.6);
+}
+.el_slot_btn:hover {
+  box-shadow: none;
+  transform: scale(0.99);
+}
+</style>
